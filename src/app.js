56000a81ef201e5b4a65351d09be28df6e621a59
Joseph Santamaria <alexis200123@outlook.com>
2024-06-30 02:52:27 -0500

    fix?

--- src/app.js
+++ src/app.js
@@ -3,12 +3,6 @@ import path from 'path';
 
 const app = express();
 
-// Configurar ruta estática
-app.use(express.static(path.join(__dirname, 'src', 'public')));
-
-// Ruta principal para index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'public', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')))
 
 export default app;
